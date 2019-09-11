ef760cf15a660ef13306c51eeb75fe8e1d0aa651
luckycyy <122825237@qq.com>
2019-09-12 02:36:32 +0800

    v1.0

--- app/src/main/java/com/cyy/qsg/MainActivity.java
+++ app/src/main/java/com/cyy/qsg/MainActivity.java
@@ -1,20 +1,13 @@
 package com.cyy.qsg;
 
-import android.content.Context;
 import android.content.Intent;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
-import android.nfc.NfcAdapter;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Looper;
-import android.os.Parcelable;
 import android.os.Vibrator;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
 
@@ -26,20 +19,27 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity {
 
     String TAG = "qsg_info";
     NfcUtils nfcUtils = null;
-
+    final OkHttpClient client = new OkHttpClient.Builder().connectTimeout(2, TimeUnit.SECONDS)
+            .readTimeout(2, TimeUnit.SECONDS).build();
+    String postPath = "http://39.105.74.138:8777/static/saveArriveNFC";
+    SoundPool soundPool;
+    int voiceId;
+    Vibrator vibrator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,62 +59,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        Log.e(TAG, "--------------1-------------");
         nfcUtils = new NfcUtils(this);
 
-    }
-
-    //在onResume中开启前台调度
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.e(TAG, "--------------2-------------");
-        //设定intentfilter和tech-list。如果两个都为null就代表优先接收任何形式的TAG action。也就是说系统会主动发TAG intent。
-        if (NfcUtils.mNfcAdapter != null) {
-            NfcUtils.mNfcAdapter.enableForegroundDispatch(this, NfcUtils.mPendingIntent, NfcUtils.mIntentFilter, NfcUtils.mTechList);
-        }
-    }
-
-
-    //在onNewIntent中处理由NFC设备传递过来的intent
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        Log.e(TAG, "--------------NFC-------------");
-        processIntent(intent);
-    }
-
-    //  这块的processIntent() 就是处理卡中数据的方法
-    public void processIntent(Intent intent) {
-        Vibrator vibrator = (Vibrator) this.getSystemService(this.VIBRATOR_SERVICE);
-        vibrator.vibrate(200);
-        playNotifySound();
-        Parcelable[] rawmsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-        NdefMessage msg = (NdefMessage) rawmsgs[0];
-        NdefRecord[] records = msg.getRecords();
-        String resultStr = new String(records[0].getPayload());
-        // 返回的是NFC检查到卡中的数据
-        Log.e(TAG, "processIntent: " + resultStr);
-        try {
-            // 检测卡的id
-            String id = NfcUtils.readNFCId(intent);
-            Log.e(TAG, "processIntent--id: " + id);
-            // NfcUtils中获取卡中数据的方法、取出学生id
-            String stuId = NfcUtils.readNFCFromTag(intent);
-            Log.e(TAG, "processIntent--result: " + stuId);
-            //todo 从响应中将学生名字拿到
-            String stuName = stuId;
-            send();
-            //Toast.makeText(getApplicationContext(),"打卡成功:" + stuName,Toast.LENGTH_LONG).show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void playNotifySound() {
-        SoundPool soundPool;
-        //实例化SoundPool
+        vibrator = (Vibrator) this.getSystemService(this.VIBRATOR_SERVICE);
 
         //sdk版本21是SoundPool 的一个分水岭
         if (Build.VERSION.SDK_INT >= 21) {
@@ -135,111 +82,103 @@ public class MainActivity extends AppCompatActivity {
              *第三个参数：int srcQuality：采样率转换器的质量。 目前没有效果。 使用0作为默认值。
              */
             soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
-
         }
-
-        final int voiceId = soundPool.load(getApplicationContext(), R.raw.finish, 1);
+        voiceId = soundPool.load(getApplicationContext(), R.raw.finish, 1);
         //异步需要等待加载完成，音频才能播放成功
         soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
             @Override
             public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
                 if (status == 0) {
-                    //第一个参数soundID
-                    //第二个参数leftVolume为左侧音量值（范围= 0.0到1.0）
-                    //第三个参数rightVolume为右的音量值（范围= 0.0到1.0）
-                    //第四个参数priority 为流的优先级，值越大优先级高，影响当同时播放数量超出了最大支持数时SoundPool对该流的处理
-                    //第五个参数loop 为音频重复播放次数，0为值播放一次，-1为无限循环，其他值为播放loop+1次
-                    //第六个参数 rate为播放的速率，范围0.5-2.0(0.5为一半速率，1.0为正常速率，2.0为两倍速率)
-                    soundPool.play(voiceId, 1, 1, 1, 0, 1);
+                    Toast.makeText(MainActivity.this,  "资源加载成功!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-/*    private static Toast toast = null;
-    public static void showToast(Context context, String text) {
-        Looper myLooper = Looper.myLooper();
-        if (myLooper == null) {
-            Looper.prepare();
-            myLooper = Looper.myLooper();
+    //在onResume中开启前台调度
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "--------------2-------------");
+        //设定intentfilter和tech-list。如果两个都为null就代表优先接收任何形式的TAG action。也就是说系统会主动发TAG intent。
+        if (NfcUtils.mNfcAdapter != null) {
+            NfcUtils.mNfcAdapter.enableForegroundDispatch(this, NfcUtils.mPendingIntent, NfcUtils.mIntentFilter, NfcUtils.mTechList);
         }
+    }
 
-        if (toast == null) {
-            toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
-            toast.setGravity(Gravity.TOP, 0, 0);
-        }
-        toast.show();
-        if ( myLooper != null) {
-            Looper.loop();
-            myLooper.quit();
-        }
-    }*/
 
-    private void send() {
-        //开启线程，发送请求
+    //在onNewIntent中处理由NFC设备传递过来的intent
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        processIntent(intent);
+    }
+
+    //  这块的processIntent() 就是处理卡中数据的方法
+    public void processIntent(Intent intent) {
+        /*Parcelable[] rawmsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        NdefMessage msg = (NdefMessage) rawmsgs[0];
+        NdefRecord[] records = msg.getRecords();
+        String resultStr = new String(records[0].getPayload());
+        // 返回的是NFC检查到卡中的数据
+        Log.e(TAG, "processIntent: " + resultStr);*/
+        try {
+            // 检测卡的id
+            //String id = NfcUtils.readNFCId(intent);
+            //Log.e(TAG, "processIntent--id: " + id);
+            // NfcUtils中获取卡中数据的方法、取出学生id
+            String stuId = NfcUtils.readNFCFromTag(intent);
+            sendGetRequestAsync(stuId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            vibrator.vibrate(2000);
+            Toast.makeText(MainActivity.this,  "打卡失败", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void playNotifySound() {
+        //第一个参数soundID
+        //第二个参数leftVolume为左侧音量值（范围= 0.0到1.0）
+        //第三个参数rightVolume为右的音量值（范围= 0.0到1.0）
+        //第四个参数priority 为流的优先级，值越大优先级高，影响当同时播放数量超出了最大支持数时SoundPool对该流的处理
+        //第五个参数loop 为音频重复播放次数，0为值播放一次，-1为无限循环，其他值为播放loop+1次
+        //第六个参数 rate为播放的速率，范围0.5-2.0(0.5为一半速率，1.0为正常速率，2.0为两倍速率)
+        soundPool.play(voiceId, 1, 1, 1, 0, 1);
+    }
+    //https://blog.csdn.net/qq_38340601/article/details/81837660 重新参考这个
+
+    private void sendGetRequestAsync(String stuId) {
+
+        final Request request = new Request.Builder()
+                .get()
+                .url(postPath+"?stuId="+stuId)
+                .build();
+
         new Thread(new Runnable() {
             @Override
             public void run() {
-                HttpURLConnection connection = null;
-                BufferedReader reader = null;
+                Response response;
                 try {
-                    URL url = new URL("http://www.163.com");
-                    connection = (HttpURLConnection) url.openConnection();
-                    //设置请求方法
-                    connection.setRequestMethod("GET");
-                    //设置连接超时时间（毫秒）
-                    connection.setConnectTimeout(5000);
-                    //设置读取超时时间（毫秒）
-                    connection.setReadTimeout(5000);
-
-                    //返回输入流
-                    InputStream in = connection.getInputStream();
-
-                    //读取输入流
-                    reader = new BufferedReader(new InputStreamReader(in));
-                    StringBuilder result = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        result.append(line);
+                    response = client.newCall(request).execute();
+                    //todo 看看网络超时怎么搞
+                        if (response.isSuccessful()) {
+                        final String rs = response.body().string();
+                        vibrator.vibrate(200);
+                        playNotifySound();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this,  "打卡成功:"+rs, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } else {
+                        throw new IOException("Unexpected code " + response);
                     }
-                    show(result.toString());
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (ProtocolException e) {
-                    e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
-                } finally {
-                    if (reader != null) {
-                        try {
-                            reader.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    if (connection != null) {//关闭连接
-                        connection.disconnect();
-                    }
                 }
             }
         }).start();
+
     }
-
-    /**
-     * 展示
-     *
-     * @param result
-     */
-    private void show(final String result) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-               //textView.setText(result);
-                Toast.makeText(getApplicationContext(), "resp:"+result, Toast.LENGTH_LONG);
-            }
-        });
-    }
-
-
-    //https://blog.csdn.net/qq_38340601/article/details/81837660 重新参考这个
 }
